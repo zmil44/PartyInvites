e263394b5c27e1036508f24e8aaa4b15f3202b04
zmil44 <zachary.j.miller@outlook.com>
2018-09-21 11:24:31 -0400

    code for PartyInvite Lab

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string timeOfDay;
+            if (hour < 12)
+            {
+                timeOfDay = "Good Morning";
+            }
+            else if (hour < 18)
+            {
+                timeOfDay = "Good Afternoon";
+            }
+            else
+            {
+                timeOfDay = "Good Evening";
+            }
+            ViewBag.Greeting = timeOfDay;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+ 
+    }
+}
